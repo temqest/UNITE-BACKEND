34be25d57e9219587aba257dcef2cb3bb40ad903
Pkv562 <pvillamer@gbox.adnu.edu.ph>
2025-11-15 14:26:13 +0800

    [UDF-017] Fix :Clean up some uncessary files

--- unite-testing-frontend/components/SidebarClient.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import Sidebar from "./Sidebar";
-
-export default function SidebarClient() {
-  // Thin client wrapper so `app/layout.tsx` (a server component) can import
-  // a client-rendered sidebar without using `dynamic(..., { ssr: false })`.
-  return <Sidebar />;
-}
